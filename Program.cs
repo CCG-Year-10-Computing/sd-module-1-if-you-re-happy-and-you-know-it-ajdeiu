dc4cc327063260749ab769e26d99f86eb0ed791c
ali.pun <ali.pun@S26-5CG538198G>
2026-02-11 12:23:53 +1100

    started project

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
 ﻿// Program Name: 
 // Created on: 
-// Author: 
+// Author: alicia
 
+Console.WriteLine("please type in your % grade (from 1 to 100)");
 
+int grade = Convert.ToInt32(Console.ReadLine());
+
+if (grade > 100 || grade < 1)
+{
+    Console.WriteLine("please put in a number from 1 to 100");
+}
+else if (grade <= 100)
+{
+    Console.WriteLine("Congradulations, you got an A!!");
+}
+else if (grade <= 80 || grade >= 100)
+{
+    Console.WriteLine("you got a B");
+}
